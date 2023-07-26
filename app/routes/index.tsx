9204e94e31a527ef07f2a0975a9190a9c819a551
ezegavilan <ezegavilan.developer@gmail.com>
2023-07-26 14:46:15 -0300

    Change meta title on /

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,7 +10,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => {
   return {
-    title: "New Remix App",
+    title: "Home",
     name: "description", content: "Welcome to Remix!"
   }
 };
